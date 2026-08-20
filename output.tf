b7bc92bf33ae7c6ef137f18bd6714e784d179c47
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-08-20 19:07:12 -0400

    output file

--- output.tf
+++ output.tf
@@ -1,3 +1,12 @@
 output "s3_name" {
-  value = aws_s3_bucket.s3_bucket
+  value = aws_s3_bucket.s3_bucket.bucket
+}
+
+output "s3_id" {
+  value = aws_s3_bucket.s3_bucket.id
+}
+
+output "s3_arn" {
+  value = aws_s3_bucket.s3_bucket.arn
+  description = "ARN of s3 bucket"
 }
