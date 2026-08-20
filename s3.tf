6cde94c58b51480a5b5c8c70123dfcbe6977c372
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-08-20 13:33:18 -0400

    s3 bucket resource block

---
+++ s3.tf
@@ -0,0 +1,15 @@
+resource "random_string" "random" {
+  length = 6
+  special = false
+  upper = false
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "krutarth-tf-bucket-${random_string.result}"
+
+  tags = {
+    Name = "My Bucket"
+    Environment = "Dev"
+    Creator = "Krutarth"
+  }
+}
